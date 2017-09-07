bae9cfd7fc08f60882138e4678d97aa2aa89f714
Christopher Blanchard <cablanchard@gmail.com>
2017-09-07 09:39:11 +0100

    Fix target=_blank links

--- app/sidebar.js
+++ app/sidebar.js
@@ -44,11 +44,11 @@ class Sidebar extends React.Component {
 								<i className="fa fa-life-ring"></i> <span>Support</span> <i className="fa fa-angle-left pull-right"></i>
 							</a>
 							<ul className="treeview-menu">
-								<li><a href="https://chat.ideal-postcodes.co.uk/" target="_blank"><i className="fa fa-commenting-o"></i> Developer Chat</a></li>
+								<li><a href="https://chat.ideal-postcodes.co.uk/" target="_blank" rel="noopener noreferrer"><i className="fa fa-commenting-o"></i> Developer Chat</a></li>
 								<li><a href="mailto:support@ideal-postcodes.co.uk"><i className="fa fa-envelope-o"></i> Email</a></li>
 								<li><a href="tel:+4408458620898"><i className="fa fa-phone"></i> 0845 862 0898</a></li>
-								<li><a href="https://twitter.com/idealpostcodes" target="_blank"><i className="fa fa-twitter"></i> Twitter</a></li>
-								<li><a href="https://github.com/ideal-postcodes/ideal_postcodes_status_page" target="_blank"><i className="fa fa-github"></i> Github</a></li>
+								<li><a href="https://twitter.com/idealpostcodes" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i> Twitter</a></li>
+								<li><a href="https://github.com/ideal-postcodes/ideal_postcodes_status_page" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i> Github</a></li>
 							</ul>
 						</li>
 					</ul>
